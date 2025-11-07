85681395860f898e1475eff9551cd9656f543193
henichard lucas <henichard@lan.sio.lyceefulbert.fr>
2025-11-07 16:45:52 +0100

    2 SQL request and starting of drawing function (should be called at the end of testButton for now)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const cellSize = 50;  // Size of cells in px
+
 function playButton_onClick()
 {
     window.open("pages/main.php");  // open main game page
@@ -6,6 +8,7 @@ function playButton_onClick()
     
 function mainPhp_onLoad()
 {
+    // display test
     canvas = document.getElementById("mainCanvas");
     ctx = canvas.getContext("2d");
         
@@ -13,31 +16,67 @@ function mainPhp_onLoad()
     ctx.fillRect(0, 0, 150, 75);    
 }
 
-function testButton_onClick()
+async function testButton_onClick()
 {
-    // Source - https://stackoverflow.com/a
-    // Posted by JuNas
-    // Retrieved 2025-11-07, License - CC BY-SA 4.0
+    // Cell id and description
+    var cellId = new Array();
+    var cellType = new Array();
 
-    /*
-    fetch('../js/test.php', {
-        method: 'GET',
-    }).then(response => response.json() ).then(result=>{
-    alert(result);
-    });*/
+    // ways between cells
+    var cell1 = new Array();
+    var cell2 = new Array();
+    var cell1Face = new Array();
+    var cell2Face = new Array();
+    var doorType = new Array();
 
-    var u = fetch("http://localhost/js/test.php")
-        .then(response => {
-            if (!response.ok) throw new Error("Erreur réseau");
-            return response.json(); // <-- transforme le flux JSON en objet JS
+    await fetch("http://localhost/js/getHallways.php")
+        .then(response =>
+        {
+            if (!response.ok) throw new Error("Network error");
+            return response.json(); // <-- transform json stream to js object
         })
-        .then(data => {
-            console.log("Réponse SQL :", data.type);
-            // Exemple : parcourir les résultats
-            data.forEach(row => console.log(row.id, row.type));
-        })
-        .catch(error => console.error("Erreur :", error));
+        .then(data => 
+        {
+            console.log("Réponse SQL :", data);
+            
+            // loop through result
+            for (let i = 0; i < data.length; i++)
+            {
+                cellId[i] = data[i].id;
+                cellType[i] = data[i].type;
+            }
 
-    //console.log(u.type);
-    //window.open("js/test.php");
+            //drawMap(idArr, typeArr);
+
+        })
+        .catch(error => console.error("Error :", error));
+        
+
+    await fetch("http://localhost/js/getDoors.php")
+        .then(response => 
+        {
+            if (!response.ok) throw new Error ("Network error");
+            return response.json();
+        })
+        .then(data =>
+        {
+            console.log("SQL answer!", data);
+
+            // loop through result
+            for (let i = 0; i < data.length; i++)
+            {
+                cell1[i] = data[i].couloir1;
+                cell2[i] = data[i].couloir2;
+                cell1Face[i] = data[i].position1;
+                cell2Face[i] = data[i].position2;
+                doorType[i] = data[i].type;
+            }
+        })
+        .catch(error => console.error("Error: ", error));
+
+}
+
+function drawMap(idArr, typeArr, cell1, cell2, cell1Face, cell2Face, doorType)
+{
+    
 }
