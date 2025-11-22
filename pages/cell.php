7c508e0b01bfec71ca44ac33a0a78c90d10cd8fc
= <=>
2025-11-22 19:33:26 +0100

    Refactored the whole code, separated game logic into 4 files mainCell.js, playerMovement.js, mazeData.js and mazeREnder.js

--- pages/cell.php
+++ pages/cell.php
@@ -1,6 +1,6 @@
 <?php
     $cellId = isset($_GET['id']) ? intval($_GET['id']) : -1;
-    // TODO: add key help to know which key do what (zqsd)
+    // TODO: add key help to know which key do what (zqsd and space)
     // TODO: show how many keys the player has if he has any (in js prolly)
 ?>
 
@@ -17,7 +17,8 @@
         </h1>
         
         <canvas id="cellCanvas" width="300" height="300" style="border:1px solid black;"></canvas>
-        <script src="../js/cell.js" defer></script>
+        <script type="module" src="../js/mainCell.js" defer></script>
+        <script type="module" src="../js/playerMovement.js" defer></script>
         
     </body>
 </html>
